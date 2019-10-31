ed451d7b8b0a64d360d04be011739aa8aceee04a
Yudi Andela <yudhi.andhela@gmail.com>
2019-10-31 21:56:25 +0700

    Menambahkan route profile di picture sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,7 +65,7 @@
                             alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+                        <a href="{{ route('profile') }}" class="d-block">{{ Auth::user()->name }}</a>
                     </div>
                 </div>
 
